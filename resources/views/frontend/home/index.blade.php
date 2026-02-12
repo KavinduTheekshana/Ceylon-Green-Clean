1a7be1aff7eca6fd89cee947b83500b998766c54
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-12 12:48:54 +0530

    Add why choose us section

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -4,6 +4,7 @@
     @include('frontend.home.slider')
     @include('frontend.home.about')
     @include('frontend.home.services')
+    @include('frontend.home.chooseUs')
     @include('frontend.home.testimonial')
     @include('frontend.home.count')
     @include('frontend.home.trust')
