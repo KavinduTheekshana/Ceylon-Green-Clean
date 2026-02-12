cff8f4f5540e4decb5e617e6bf85fa556206dc05
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-12 13:53:30 +0530

    Update contact info section

--- resources/views/frontend/contact/info.blade.php
+++ resources/views/frontend/contact/info.blade.php
@@ -4,15 +4,15 @@
             <div class="col-md-3 wow fadeInRight" data-wow-delay=".2s">
                 <div class="text-center">       
                     <i class="mb-3 bg-color text-light fs-32 p-30 circle fa fa-phone"></i>
-                    <h5 class="mb-0">Call Us</h5>
+                    <h5 class="mb-0">Support / Inquiries</h5>
                     <a href="tel:+94756745331" class="text-decoration-none">(+94) 75 674 53 31</a>
                 </div>
             </div>
             <div class="col-md-3 wow fadeInRight" data-wow-delay=".4s">
-                <div class="text-center">                  
-                    <i class="mb-3 bg-color text-light fs-32 p-30 circle fa fa-user"></i>    
-                    <h5 class="mb-0">Support / Inquiries</h5>
-                    <a href="tel:+94768002660" class="text-decoration-none">(+94) 76 800 26 60</a>
+                <div class="text-center">
+                    <i class="mb-3 bg-color text-light fs-32 p-30 circle fa fa-map-marker-alt"></i>
+                    <h5 class="mb-0">Service Area</h5>
+                    <p class="mb-0">Kurunegala, Wariyapola, Alawwa, Kandy</p>
                 </div>
             </div>
             <div class="col-md-3 wow fadeInRight" data-wow-delay=".6s">
