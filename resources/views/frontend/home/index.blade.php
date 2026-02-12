6fc9c6a23e22b7712d1a7304fe55e361881d02c3
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-12 11:36:24 +0530

    Update home page

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     @include('frontend.home.slider')
     @include('frontend.home.services')
-    @include('frontend.home.pricing')
     @include('frontend.home.about')
     @include('frontend.home.testimonial')
     @include('frontend.home.count')
