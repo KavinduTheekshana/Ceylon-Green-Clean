1653d08db1973e823d0896d95aa66eeba90a3847
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-12 09:55:37 +0530

    Update  Testimonials setion

--- resources/views/frontend/home/testimonial.blade.php
+++ resources/views/frontend/home/testimonial.blade.php
@@ -1,8 +1,8 @@
 <section class="border-top bg-light">
     <div class="container">
         <div class="row g-4">
-            <div class="col-lg-6 offset-lg-3 text-center">
-                <div class="subtitle wow fadeInUp mb-3">Testimonials</div>
+            <div class="text-center col-lg-6 offset-lg-3">
+                <div class="mb-3 subtitle wow fadeInUp">Testimonials</div>
                 <h2 class="mb-4 wow fadeInUp" data-wow-delay=".2s">Our Happy Customers</h2>
             </div>
         </div>
@@ -14,12 +14,18 @@
                     <div class="item">
                         <div class="relative p-2">
                             <div class="relative">
-                                <img class="relative z-2 w-80px mb-3 rounded-1 testimonial-image"
+                            @if(!empty($testimonial->image) && file_exists(public_path('storage/' . $testimonial->image)))
+                                <img class="relative mb-3 z-2 w-80px rounded-1 testimonial-image"
                                     alt="{{ $testimonial->name }}"
                                     src="{{ $testimonial->image ? asset('storage/' . $testimonial->image) : asset('frontend/images/placeholder.png') }}">
+                            @else
+                                <img class="relative mb-3 z-2 w-80px rounded-1 testimonial-image"
+                                    alt="{{ $testimonial->name }}"
+                                     src="{{ asset('frontend/images/placeholder.png') }}">
+                            @endif
                             </div>
                             <div class="mt-4 text-dark fw-600">{{ $testimonial->name }}</div>
-                            <div class="de-rating-ext mb-3">
+                            <div class="mb-3 de-rating-ext">
                                 <span class="d-stars">
                                     <i class="fa fa-star"></i>
                                     <i class="fa fa-star"></i>
