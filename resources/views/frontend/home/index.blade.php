b1f8b639ad661c0d17e37407e34d46ad95e3fa61
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-12 13:40:00 +0530

    Add CTA section

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -6,11 +6,8 @@
     @include('frontend.home.services')
     @include('frontend.home.chooseUs')
     @include('frontend.home.testimonial')
+    @include('frontend.home.cta')
     @include('frontend.home.count')
     @include('frontend.home.trust')
-
-
-
-
 
 @endsection
